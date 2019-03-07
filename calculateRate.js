e62dfb4e19e7abd92514eb3638f2961cda3ebe60
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-03-06 19:14:03 -0700

    adding another change to postage app

--- calculateRate.js
+++ calculateRate.js
@@ -9,7 +9,7 @@ module.exports = function calculateRate(req, res) {
     let FCPSR = new Map([[4, 3.66], [8, 4.39], [12, 5.19], [13, 5.71]]);
     let allPostage = new Map([['LS', LS], ['LM', LM], ['LEF', LEF], ['FCPSR', FCPSR]]);
 
-    let finalCost = 0;
+    let finalCost = -1;
 
     //The only way to quit out of array.prototype.forEach is to throw an exception.
     try {
